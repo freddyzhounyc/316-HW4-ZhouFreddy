456fc81171940536703947863ccc0600885ff46b
Freddy Zhou <freddyzhou06@gmail.com>
2025-11-04 17:02:04 -0500

    Add unit tests implemented with vitest and fix MongoDBManager save function.

--- server/test/mongo.test.js
+++ server/test/mongo.test.js
@@ -1,9 +1,13 @@
 import { beforeAll, beforeEach, afterEach, afterAll, expect, test } from 'vitest';
-const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
-const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ 
+    path: __dirname + '/../.env' 
+});
+const { MongoDBManager, MongoPlaylist, MongoUser, resetMongo } = require('../db/mongodb/index');
+const { PostgreSQLManager, PostgresPlaylist, PostgresUser, resetPostgres } = require('../db/postgresql/index');
 
 /**
- * Vitest test script for the Playlister app's Mongo Database Manager. Testing should verify that the Mongo Database Manager 
+ * Vitest test script for the Playlister app's Database Manager. Testing should verify that the Database Manager 
  * will perform all necessarily operations properly.
  *  
  * Scenarios we will test:
@@ -18,20 +22,43 @@ const mongoose = require('mongoose')
 /**
  * Executed once before all tests are performed.
  */
+
+let dbManager = null;
+let user = null;
+let playlist = null;
+
 beforeAll(async () => {
-    // SETUP THE CONNECTION VIA MONGOOSE JUST ONCE - IT IS IMPORTANT TO NOTE THAT INSTEAD
-    // OF DOING THIS HERE, IT SHOULD BE DONE INSIDE YOUR Database Manager (WHICHEVER)
-    await mongoose
-        .connect(process.env.DB_CONNECT, { useNewUrlParser: true })
-        .catch(e => {
-            console.error('Connection error', e.message)
-        })
+    if (process.env.DB_CHOICE.toLowerCase() === "mongodb") {
+        dbManager = new MongoDBManager();
+        user = MongoUser;
+        playlist = MongoPlaylist;
+    } else if (process.env.DB_CHOICE.toLowerCase() === "postgres") {
+        dbManager = new PostgreSQLManager();
+        user = PostgresUser;
+        playlist = PostgresPlaylist;
+    } else
+        throw new Error("Invalid .env value for DB_CHOICE!");
+
+    try {
+        dbManager.connect();
+    } catch (err) {
+        console.error("Connection Error", err.message);
+    }
 });
 
 /**
  * Executed before each test is performed.
  */
-beforeEach(() => {
+beforeEach(async () => {
+    try {
+        // reset db to default values in json file
+        if (process.env.DB_CHOICE.toLowerCase() === "mongodb")
+            await resetMongo();
+        else // else can be used as other choices besides mongodb or postgres has been taken care of
+            await resetPostgres();
+    } catch (err) {
+        console.error("Failed to reset datbase!", err.message);
+    }
 });
 
 /**
@@ -43,60 +70,224 @@ afterEach(() => {
 /**
  * Executed once after all tests are performed.
  */
-afterAll(() => {
+afterAll(async () => {
+    try {
+        // reset db to default values in json file
+        if (process.env.DB_CHOICE.toLowerCase() === "mongodb")
+            await resetMongo();
+        else // else can be used as other choices besides mongodb or postgres has been taken care of
+            await resetPostgres();
+    } catch (err) {
+        console.error("Failed to reset datbase!", err.message);
+    }
 });
 
 /**
  * Vitest test to see if the Database Manager can get a User.
  */
-test('Test #1) Reading a User from the Database', () => {
-    // FILL IN A USER WITH THE DATA YOU EXPECT THEM TO HAVE
+test('Test #1) Reading a User from the Database With Criteria', async () => {
     const expectedUser = {
-        // FILL IN EXPECTED DATA
+        firstName: "Joe",
+        lastName: "Shmo",
+        email: "joe@shmo.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: [
+            "68f524b611e0f3901871f164",
+            "68f526c1ec1ea2ad00a6fae0"
+        ]
     };
 
     // THIS WILL STORE THE DATA RETRUNED BY A READ USER
-    const actualUser = {};
+    let actualUser = {};
 
     // READ THE USER
-    // actualUser = dbManager.somethingOrOtherToGetAUser(...)
+    actualUser = await dbManager.readOne(user, { email: expectedUser.email }); // Function to TEST!
 
     // COMPARE THE VALUES OF THE EXPECTED USER TO THE ACTUAL ONE
-    expect(expectedUser.firstName, actualUser.firstName)
-    expect(expectedUser.lastName, actualUser.lastName);
+    expect(actualUser.firstName).toBe(expectedUser.firstName);
+    expect(actualUser.lastName).toBe(expectedUser.lastName);
     // AND SO ON
 });
 
 /**
  * Vitest test to see if the Database Manager can create a User
  */
-test('Test #2) Creating a User in the Database', () => {
+test('Test #2) Creating a User in the Database', async () => {
     // MAKE A TEST USER TO CREATE IN THE DATABASE
-    const testUser = {
-        // FILL IN TEST DATA, INCLUDE AN ID SO YOU CAN GET IT LATER
-    };
+    let testUser = {
+        firstName: "Chris",
+        lastName: "Brown",
+        email: "cbrown@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: []
+    }
 
     // CREATE THE USER
-    // dbManager.somethingOrOtherToCreateAUser(...)
+    await dbManager.save(user, testUser); // Function to TEST!
 
     // NEXT TEST TO SEE IF IT WAS PROPERLY CREATED
 
     // FILL IN A USER WITH THE DATA YOU EXPECT THEM TO HAVE
     const expectedUser = {
-        // FILL IN EXPECTED DATA
+        firstName: "Chris",
+        lastName: "Brown",
+        email: "cbrown@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: []
     };
 
     // THIS WILL STORE THE DATA RETRUNED BY A READ USER
-    const actualUser = {};
+    let actualUser = {};
 
     // READ THE USER
-    // actualUser = dbManager.somethingOrOtherToGetAUser(...)
+    actualUser = await dbManager.readOne(user, { email: testUser.email });
 
     // COMPARE THE VALUES OF THE EXPECTED USER TO THE ACTUAL ONE
-    expect(expectedUser.firstName, actualUser.firstName)
-    expect(expectedUser.lastName, actualUser.lastName);
+    expect(actualUser.firstName).toBe(expectedUser.firstName);
+    expect(actualUser.lastName).toBe(expectedUser.lastName);
     // AND SO ON
-
 });
 
 // THE REST OF YOUR TEST SHOULD BE PUT BELOW
+test("Test #3) Reading a User by ID in the Database", async () => {
+    const testUser = {
+        firstName: "Steph",
+        lastName: "Curry",
+        email: "chefcurry@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: []
+    };
+
+    const savedUser = await dbManager.save(user, testUser);
+
+    const expectedUser = {
+        firstName: "Steph",
+        lastName: "Curry",
+        email: "chefcurry@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: []
+    }
+
+    const actualUser = await dbManager.readOneById(user, savedUser._id || savedUser.id); // Function to TEST!
+
+    expect(actualUser.firstName).toBe(expectedUser.firstName);
+    expect(actualUser.lastName).toBe(expectedUser.lastName);
+    expect(actualUser.email).toBe(expectedUser.email);
+});
+
+test("Test #4) Reading All Playlists With A Certain Criteria", async () => {
+    const testUser = {
+        firstName: "Seth",
+        lastName: "Curry",
+        email: "chefcurry@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: [] // disregarded for sequelize
+    }
+    const testPlaylist1 = {
+        name: "MyPlaylist",
+        ownerEmail: "chefcurry@gmail.com",
+        songs: []
+    };
+    const testPlaylist2 = {
+        name: "MySecondPlaylist",
+        ownerEmail: "chefcurry@gmail.com",
+        songs: []
+    };
+
+    await dbManager.save(user, testUser);
+    await dbManager.save(playlist, testPlaylist1);
+    await dbManager.save(playlist, testPlaylist2);
+
+    const resultArray = await dbManager.readAll(playlist, { ownerEmail: testUser.email }); // Function to TEST!
+    expect(resultArray).toHaveLength(2);
+});
+
+test("Test #5) Update a Playlist in the Database", async () => {
+    const newPlaylist = {
+        ownerEmail: "joe@shmo.com",
+        name: "Three",
+        songs: []
+    }
+
+    const createdPlaylist = await dbManager.save(playlist, newPlaylist);
+
+    let updatePlaylist = {
+        ownerEmail: "joe@shmo.com",
+        name: "Twenty One",
+        songs: []
+    };
+    if (process.env.DB_CHOICE.toLowerCase() === "postgres") {
+        updatePlaylist = {
+            ...updatePlaylist,
+            id: createdPlaylist.id || createdPlaylist._id
+        }
+    } else if (process.env.DB_CHOICE.toLowerCase() === "mongodb") {
+        updatePlaylist = {
+            ...updatePlaylist,
+            _id: createdPlaylist.id || createdPlaylist._id
+        }
+    }
+
+    const savedPlaylist = await dbManager.save(playlist, updatePlaylist); // Function to TEST!
+
+    let expectedPlaylist = {
+        ownerEmail: "joe@shmo.com",
+        name: "Twenty One",
+        songs: []
+    }
+    if (process.env.DB_CHOICE.toLowerCase() === "postgres") {
+        expectedPlaylist = {
+            ...expectedPlaylist,
+            id: createdPlaylist.id || createdPlaylist._id
+        }
+    } else if (process.env.DB_CHOICE.toLowerCase() === "mongodb") {
+        expectedPlaylist = {
+            ...expectedPlaylist,
+            _id: createdPlaylist.id || createdPlaylist._id
+        }
+    }
+
+    const actualPlaylist = await dbManager.readOneById(playlist, (savedPlaylist._id || savedPlaylist.id));
+    
+    expect(actualPlaylist.name).toBe(expectedPlaylist.name);
+    expect(actualPlaylist.songs).toHaveLength(expectedPlaylist.songs.length);
+});
+
+test("Test #6) Test if Connection is Established", async () => {
+    const actualValue = await dbManager.connect(); // Function to TEST!
+    expect(actualValue).toBeNull();
+})
+
+test("Test #7) Test Deleting a User By ID", async () => {
+    const testUser = {
+        firstName: "Steph",
+        lastName: "Curry",
+        email: "chefcurry@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: [] // disregarded for sequelize
+    }
+    const savedUser = await dbManager.save(user, testUser);
+    const readSavedUser = await dbManager.readOneById(user, savedUser.id || savedUser._id);
+    expect(savedUser.email).toBe(readSavedUser.email);
+
+    await dbManager.deleteById(user, savedUser.id || savedUser._id); // Function to TEST!
+    const readDeletedUser = await dbManager.readOneById(user, savedUser.id || savedUser._id);
+    expect(readDeletedUser).toBeNull();
+});
+
+test("Test #8) Test Deleting a User By Criteria", async () => {
+    const testUser = {
+        firstName: "Steph",
+        lastName: "Curry",
+        email: "chefcurry@gmail.com",
+        passwordHash: "$2a$10$dPEwsAVi1ojv2RfxxTpZjuKSAbep7zEKb5myegm.ATbQ4sJk4agGu",
+        playlists: [] // disregarded for sequelize
+    }
+    const savedUser = await dbManager.save(user, testUser);
+    const readSavedUser = await dbManager.readOneById(user, savedUser.id || savedUser._id);
+    expect(savedUser.email).toBe(readSavedUser.email);
+
+    await dbManager.delete(user, { email: "chefcurry@gmail.com" });
+    const readDeletedUser = await dbManager.readOneById(user, savedUser.id || savedUser._id);
+    expect(readDeletedUser).toBeNull();
+});
